e475c587a828d0a7abeb6f3c08bc0d82aea47c53
unknown <leeviva@gmail.com>
2022-01-26 16:08:38 +0200

    poistettu turha import

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
-import reactDom from 'react-dom';
+
 
 export default function App() {
 
